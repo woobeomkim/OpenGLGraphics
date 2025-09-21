be6841bf8f30df1241626d11a0f3789d636337de
woobum <woobum72@gmail.com>
2025-09-22 00:27:23 +0900

    초기 커밋.

---
+++ OpenGL/src/Application.cpp
@@ -0,0 +1,78 @@
+//http://glew.sourceforge.net/
+//사이트에서 GLEW 바이너리 버전다운로드. 필요 라이브러리 dependencies에 복사 후 설정
+//http://glew.sourceforge.net/basic.html
+//아래 예제 코드실행 확인 
+
+//GLEW_STATIC Define 필요
+#include <GL/glew.h> //glfw보다 먼저 include해야 함
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main(void)
+{
+	GLFWwindow* window;
+
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
+
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+
+	// glfwMakeContextCurrent가 호출된 후에 glewInit이 수행되어야 함
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "Error\n";
+	}
+
+	std::cout << glGetString(GL_VERSION) << std::endl; //내 플랫폼의 GL_Version 출력해보기
+
+	//glEnable(GL_CULL_FACE);
+	float position[6] = {
+		0.0f, 0.5f,
+		0.5f, -0.5f,
+		-0.5f, -0.5f
+	};
+
+	unsigned int bufferID;
+	glGenBuffers(1, &bufferID);
+	glBindBuffer(GL_ARRAY_BUFFER, bufferID); // <-- BIND == ACTIVATE
+	glBufferData(GL_ARRAY_BUFFER, // 실제 CPU -> GPU
+		sizeof(float) * 6,
+		position,
+		GL_STATIC_DRAW);
+
+
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Render here */
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glDrawArrays(GL_TRIANGLES, 0, 3); // draw call
+
+		//// 삼각형 그리는 Legacy 코드 추가
+		//glBegin(GL_TRIANGLES);
+		//glVertex2f(-0.5f, -0.5f);
+		//glVertex2f( 0.0f,  0.5f);
+		//glVertex2f( 0.5f, -0.5f);
+		//glEnd();
+
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
