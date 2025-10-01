cb437f315fe0a8c5332ecd7a5cdeba95cedd2305
woobum <woobum72@gmail.com>
2025-10-01 13:11:58 +0900

    Camera Contorl

--- OpenGL/res/shaders/Basic.shader
+++ OpenGL/res/shaders/Basic.shader
@@ -2,14 +2,18 @@
 #version 330 core
 
 layout(location = 0) in vec4 position; 
+//layout(location = 1) in vec2 texCoord;
 
-uniform mat4 u_Model;
-uniform mat4 u_Proj;
+out vec2 v_TexCoord;
+
+uniform mat4 u_Projection;
+uniform mat4 u_View;
+uniform vec4 u_Color; //uniform에는 id가 할당됨
 
 void main()
 {
-	gl_Position = u_Proj * u_Model * position;  // gl_Position에 넣어야 할 정보 : Clip space coordinate
-
+	gl_Position = u_Projection * u_View * position;  // gl_Position에 넣어야 할 정보 : Clip space coordinate
+	//v_TexCoord = texCoord;
 };
 
 #shader fragment
@@ -17,7 +21,13 @@ void main()
 
 layout(location = 0) out vec4 color;
 
+in vec2 v_TexCoord; //버텍스 셰이더에서 넘겨받은 데이터
+
+uniform sampler2D u_Texture; //texture는 sampler2D 타입
+
 void main()
 {
-	color = vec4(1.0f,0.0f,0.0f,1.0f); 
+	//vec4 texColor = texture(u_Texture, v_TexCoord); //texture 함수는 texcoord위치의 픽셀 색상 값을 정의된 방법에 따라 샘플링
+	//color = texColor; //이제 텍스처 이미지에서 샘플링한 색상을 fragment 색상으로 결정
+	color = vec4(1.0f,0.0f,0.0f,1.0f);
 };
--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -18,6 +18,7 @@
 #include "Shader.h"
 #include "VertexBufferLayout.h"
 #include "Window.h"
+#include "Camera.h"
 
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
@@ -77,15 +78,18 @@ int main(void)
 
 	IndexBuffer ib{ indices,6 };
 
-	// modle view projection matrix 정의
-	glm::mat4 model = GetTranslationMatrix(0.0f, 0.0f, 0.0f);
-	glm::mat4 proj = GetProjectionMatrix(3.14F / 3, 640 / 480 , 1.0f, 100.0f);
+	//yaw 값이 0일때는 front가 [1,0,0]이므로, yaw를 90으로 해서 초기 front가 [0,0,-1]이 되도록 함
+	Camera camera{ glm::vec3{0.0f,0.0f,5.0f}, glm::vec3{0.0f,1.0f,0.0f}, -90.0f, 0.0f, 5.0f, 0.5f };
 
-	//glm::mat4 proj = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f);
-
+	//앞뒤 이동 효과를 올바로 보기 위해서는 perspective projection 행렬 필요
+	float aspect = (float)mainWindow.GetBufferWidth() / mainWindow.GetBufferHeight();
+	glm::mat4 proj = glm::perspective(45.0f, aspect, 0.1f, 100.0f);
 	// Shader
 	Shader shader{ "res/shaders/Basic.shader" };
 	shader.Bind();
+	shader.SetUniform4f("u_Color", 0.2f, 0.3f, 0.8f, 1.0f);
+	shader.SetUniformMat4f("u_Projection", proj);
+	shader.SetUniformMat4f("u_View", camera.calculateViewMatrix());
 
 	Renderer renderer;
 
@@ -93,22 +97,33 @@ int main(void)
 	vb.Unbind();
 	ib.Unbind();
 	shader.Unbind();
+
+	float deltaTime = 0.0f;
+	float lastTime = 0.0f;
+
 	/* Loop until the user closes the window */
 	while (!mainWindow.GetShouldClose())
 	{
+		float now = glfwGetTime(); //현재 시간
+		deltaTime = now - lastTime; //소요 시간 = 현재 시간 - 이전 프레임 시간 
+		lastTime = now;
+
+		//poll event 부분은 유저 입력에 필요하므로 남겨둠
+		glfwPollEvents();
+
+		camera.KeyControl(mainWindow.GetKeys(), deltaTime);
+		camera.MouseControl(mainWindow.GetXChange(), mainWindow.GetYChange());
+
 		/* Render here */
 		renderer.Clear();
 
 		shader.Bind();
-		shader.SetUniformMat4f("u_Model", model);
-		shader.SetUniformMat4f("u_Proj", proj);
-		
+		shader.SetUniformMat4f("u_View", camera.calculateViewMatrix()); //카메라 변화에 따라 새로 계산된 view 행렬 셰이더에 전달
+
 		renderer.Draw(va, ib, shader);
 		/* Swap front and back buffers */
 		mainWindow.SwapBuffers();
 
-		/* Poll for and process events */
-		glfwPollEvents();
 	}
 	return 0;
 }
